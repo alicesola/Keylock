fc786d7e6f4961bc76d65c7dd0e68acf55a246e7
Alice <3395239457@qq.com>
2025-09-13 14:29:31 +0800

    rename

--- data/dataIO.cpp
+++ data/dataIO.cpp
@@ -11,5 +11,17 @@ std::string readFile(const std::string &filePath)
     }
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
+    if(rename(filePath.c_str(), (filePath + ".bak").c_str())){
+        throw std::runtime_error("Could not create backup file: " + filePath + ".bak");
+    }
     return content;
+}
+void writeFile(const std::string &filePath, const std::string &serializedData){
+    std::ofstream file(filePath);
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Could not open file: " + filePath);
+    }
+    file << serializedData;
+    file.close();
 }
