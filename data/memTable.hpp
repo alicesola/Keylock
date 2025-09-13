60c226856af903af426eae1d7ef2c2843d9b3f6d
Alice <3395239457@qq.com>
2025-09-13 19:19:17 +0800

    memOP

---
+++ data/memTable.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "dataIO.hpp"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+
+class MemTable
+{
+    public:
+    explicit MemTable(const std::string &filePath) : filePath(filePath)
+    {
+        loadFromDisk();
+    }
+    passwordData* find(const std::string &site)
+    {
+        for(auto &p :data_)
+        {
+            if(p.site == site)
+            {
+                return &p;
+            }
+        }
+        return nullptr;
+    }
+    void insert(const passwordData &pd)
+    {
+        if(find(pd.site))
+        {
+            throw std::runtime_error("Entry for site already exists: " + pd.site);
+        }
+        data_.push_back(pd);
+    }
+    void update(const passwordData &pd)
+    {
+        if(passwordData* p=find(pd.site))
+        {
+            *p = pd;
+        }
+        else
+        {
+            throw std::runtime_error("No entry found for site: " + pd.site);
+        }
+    }
+    bool remove(const std::string &site)
+    {
+        auto it = std::remove_if(data_.begin(),data_.end(), [&](const passwordData &pd){ return pd.site == site; });
+        if(it == data_.end())
+        {
+            return false;
+        }
+        data_.erase(it, data_.end());
+        return true;
+    }
+    const std::vector<passwordData>& getAll() const
+    {
+        return data_;
+    }
+    void flushToDisk()
+    {
+       const std::string serializedData = serialize(data_);
+        writeFile(filePath, serializedData);
+    }
+    ~MemTable(){ flushToDisk(); }
+    private:
+    std::string filePath;
+    std::vector<passwordData> data_;
+    void loadFromDisk(){
+        const std::string rawData = readFile(filePath);
+        data_ = parse(rawData);
+    }
+};
